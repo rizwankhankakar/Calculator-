4a125e26e10927b765513ade9b44f24dd92bdfd4
rizwankhankakar <rizwankakar904@gmail.com>
2026-03-10 12:38:43 +0500

    Add files via upload

---
+++ jahad/script.js
@@ -0,0 +1,9 @@
+const currencies = [
+{ code: "USD", name: "US Dollar", flag: "🇺🇸" },
+{ code: "EUR", name: "Euro", flag: "🇪🇺" },
+{ code: "GBP", name: "British Pound", flag: "🇬🇧" },
+{ code: "JPY", name: "Japanese Yen", flag: "🇯🇵" },
+{ code: "CAD", name: "Canadian Dollar", flag: "🇨🇦" },
+{ code: "AUD", name: "Australian Dollar", flag: "🇦🇺" },
+{ code: "CHF", name: "Swiss Franc", flag: "🇨🇭" }
+];
